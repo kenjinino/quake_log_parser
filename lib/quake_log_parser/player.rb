301c0053598670611de469ae498d0ce45a07c3a8
Edson Ninomiya <kenjinino07@gmail.com>
2013-10-14 15:23:50 -0300

    Added some player's methods and tests

--- lib/quake_log_parser/player.rb
+++ lib/quake_log_parser/player.rb
@@ -10,5 +10,15 @@ module QuakeLogParser
 
     attr_reader :id
     attr_accessor :name, :kills, :deaths
+
+    def killed(killed_player)
+      @kills += 1
+
+      killed_player.deaths += 1
+    end
+
+    def killed_by_world
+      @kills -= 1
+    end
   end
 end
--- spec/lib/quake_log_parser/player_spec.rb
+++ spec/lib/quake_log_parser/player_spec.rb
@@ -2,11 +2,10 @@ require 'spec_helper'
 
 describe QuakeLogParser::Player do
   
-  before do
-    @player = QuakeLogParser::Player.new(1, "dummy_player")
-  end
+  let(:player) { QuakeLogParser::Player.new(1, "dummy_player") }
+  let(:killed_player) { QuakeLogParser::Player.new(2, "killed_player") }
 
-  subject { @player }
+  subject { player }
 
   it { should respond_to :id }
   it { should respond_to :name }
@@ -14,7 +13,29 @@ describe QuakeLogParser::Player do
   it { should respond_to :deaths }
 
   it "should have zero kills and deaths after created" do
-    @player.kills.should eql(0)
-    @player.deaths.should eql(0)
+    player.kills.should eql(0)
+    player.deaths.should eql(0)
+  end
+
+  describe "#killed" do
+  
+    before do
+      player.killed(killed_player)
+    end
+
+    it "should have increased player's kills count" do
+      player.kills.should eql(1)
+    end 
+
+    it "killed player's death count should have increased" do
+      killed_player.deaths.should eql(1)
+    end 
+  end
+
+  describe "#killed_by_world" do
+    it "should have kills count decreased" do
+      player.killed_by_world
+      player.kills.should eql(-1)
+    end
   end
 end
